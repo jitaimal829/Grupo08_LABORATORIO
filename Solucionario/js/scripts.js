b6f8123a7e1d3aaa60e88e547995477a35c9f5ea
Ignacio Taimal <57081975+jitaimal829@users.noreply.github.com>
2022-01-11 21:51:39 -0500

    Add files via upload

---
+++ Solucionario/js/scripts.js
@@ -0,0 +1,16 @@
+var parser, xmlDoc;
+var text = "<web><lab>" +
+"<titulo>Laboratorio Aplicacion de Tecnologias Web <titulo>" +
+"<grupo>Grupo 08</grupo>" +
+"<fecha>Enero 2022</fecha>" +
+"</lab></web>";
+
+parser = new DOMParser();
+xmlDoc = parser.parseFromString(text,"text/xml");
+
+document.getElementById("texto1").innerHTML =
+xmlDoc.getElementsByTagName("titulo")[0].childNodes[0].nodeValue;
+document.getElementById("texto2").innerHTML =
+xmlDoc.getElementsByTagName("grupo")[0].childNodes[0].nodeValue;
+document.getElementById("texto3").innerHTML =
+xmlDoc.getElementsByTagName("fecha")[0].childNodes[0].nodeValue;
